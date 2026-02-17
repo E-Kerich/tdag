f45d512a3bec81295aecae61a4c4e3695654ed4c
Emmanuel Kerich <professionalcvwritersconsultan@gmail.com>
2026-02-17 16:52:40 +0300

    fix model path

--- backend/controllers/public/aiDiscovery.controller.js
+++ backend/controllers/public/aiDiscovery.controller.js
@@ -1,4 +1,4 @@
-const AIDiscovery = require("../../models/AiDiscovery");
+const AIDiscovery = require("../../models/AIDiscovery");
 const sendEmail = require("../../utils/resend");
 
 // POST /api/ai-discovery

02befe22067bf92ef840f32c50775459164a8e95
Emmanuel Kerich <professionalcvwritersconsultan@gmail.com>
2026-03-02 00:34:59 +0300

    Admin controllers commits

--- backend/routes/admin/adminaidiscovery.routes.js
+++ backend/routes/admin/adminaidiscovery.routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const {
   getAllRequests,
   updateStatus
-} = require("../../controllers/admin/AIDiscovery.controller");
+} = require("../../controllers/admin/adminaidiscovery.controller");
 const { protect } = require("../../middleware/auth.middleware");
 
 router.use(protect);
--- backend/routes/public/publicaidiscovery.routes.js
+++ backend/routes/public/publicaidiscovery.routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const {
   submitAIDiscovery
-} = require("../../controllers/public/AIDiscovery.controller");
+} = require("../../controllers/public/publicaidiscovery.controller");
 
 router.post("/", submitAIDiscovery);
 
